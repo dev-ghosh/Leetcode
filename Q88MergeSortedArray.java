ddd524014e5f25417a472a9aae7597f06c6b80ef
Dev Ghosh <dev.work616@gmail.com>
2024-12-20 16:51:33 +0530

    Add files via upload

---
+++ Q88MergeSortedArray.java
@@ -0,0 +1,19 @@
+package LEETCODE;
+import java.util.Scanner;
+public class Q88MergeSortedArray {
+    public void merge(int[]nums1,int n,int[]nums2,int m){
+        int p1=m-1,p2=n-1,i=m+n-1;
+        while(p2>=0){
+            if(p1>=0 && nums1[p1]>nums2[p2]){
+                nums1[i--]=nums1[p1--];
+            }
+            else{
+                nums1[i--]=nums2[p2--];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
